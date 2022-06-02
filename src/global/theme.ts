0cfd9e16dd9a4bdaf2ad07562f80c9d18d0d2674
luiz <lbrebelatto@gmail.com>
2022-06-01 21:59:46 -0300

    styles: create styles global and typing

---
+++ src/global/theme.ts
@@ -0,0 +1,10 @@
+export default {
+  colors: {
+    background:'#E5E5E5',
+    background_header: '#494949',
+    white: '#FFFFFF',
+    text: '#303943',
+    grey_dark: '#212121',
+    grey_medium: '#767676'
+  }
+}
